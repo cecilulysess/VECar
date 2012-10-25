4364904c0de2372bc5c83ae1493be068a4380b1b
Julian Wu <cosmobserver@gmail.com>
2012-10-25 13:30:23 -0400

    fix commit problem. Milestone 1

--- Assets/Car.js
+++ Assets/Car.js
@@ -27,26 +27,26 @@ function Start () {
 	if(CenterOfMass != null){
 		rigidbody.centerOfMass = CenterOfMass.localPosition;
 	}
-	
+
 	topSpeed = topSpeed * 0.44704;
-	
+
 }
 
 function Update () {
 	//relativeVelocity = transform.InverseTransformDirection(rigidbody.velocity);
-	
+
 	v = Input.GetAxis("Vertical");
 	h = Input.GetAxis("Horizontal");
-	
+
 }
 
 function FixedUpdate () {
 	relativeVelocity = transform.InverseTransformDirection(rigidbody.velocity);
-	
+
 	addDrag(relativeVelocity);
-	
+
 	changeSteering(relativeVelocity);
-	
+
 	addPower(relativeVelocity);
 
 }
@@ -55,14 +55,14 @@ function addDrag (relativeVelocity : Vector3) {
 	var relativeDrag : Vector3 = new Vector3(	-relativeVelocity.x * Mathf.Abs(relativeVelocity.x), 
 												-relativeVelocity.y * Mathf.Abs(relativeVelocity.y), 
 												-relativeVelocity.z * Mathf.Abs(relativeVelocity.z) );
-	
+
 	var drag = Vector3.Scale(dragMultiplier, relativeDrag);
-	
+
 	drag.x *= topSpeed / relativeVelocity.magnitude;
-	
+
 	if(Mathf.Abs(relativeVelocity.x) < 5)
 		drag.x = -relativeVelocity.x * dragMultiplier.x;
-		
+
 
 	rigidbody.AddForce(transform.TransformDirection(drag) * rigidbody.mass * Time.deltaTime);
 }
@@ -70,16 +70,16 @@ function addDrag (relativeVelocity : Vector3) {
 function changeSteering (relativeVelocity : Vector3) {
 	var FL : WheelCollider = wheels[0].gameObject.GetComponent(WheelCollider);
 	var FR : WheelCollider = wheels[1].gameObject.GetComponent(WheelCollider);
-	
+
 	var speedAdjust : float = 1;
-	
+
 	if(relativeVelocity.z > 0){
 		speedAdjust = inverseSpeed(relativeVelocity);
 	}
-	
+
 	steerAngle = (h * maxTurn * speedAdjust);
 	steerAngle = Mathf.Clamp(steerAngle,(maxTurn * -1), maxTurn);
-	
+
 	FL.steerAngle = steerAngle;
 	FR.steerAngle = steerAngle;
 }
@@ -97,21 +97,21 @@ function addPower (relativeVelocity : Vector3) {
 	else{
 		currentEnginePower -= Time.deltaTime * 300;
 	}
-	
+
 	if(Mathf.Sign(v) > 0){
 		currentEnginePower = Mathf.Clamp(currentEnginePower,0,200);
 	}
 	else{
 		currentEnginePower = Mathf.Clamp(currentEnginePower,0,50);
 	}
-	
+
 	if(Mathf.Sign(v) == Mathf.Sign(relativeVelocity.z)){
 		throttleForce = Mathf.Sign(v) * currentEnginePower * rigidbody.mass;
 	}
 	else{
 		brakeForce = Mathf.Sign(v) * rigidbody.mass * 60;
 	}
-	
+
 	rigidbody.AddForce(transform.forward * Time.deltaTime * (throttleForce + brakeForce));
 }
 
@@ -121,7 +121,7 @@ function inverseSpeed(relativeVelocity : Vector3) : float{
 
 function speedFactor(relativeVelocity : Vector3) : float{
 	factor = 1;
-	
+
 	if(Mathf.Sign(relativeVelocity.z) > 0){ 
 		if(relativeVelocity.z < 3){
 			factor += (3/relativeVelocity.z);
@@ -131,6 +131,6 @@ function speedFactor(relativeVelocity : Vector3) : float{
 			factor = Mathf.Clamp(factor,0.5,1);
 		}
 	}
-	
+
 	return factor;
 }
