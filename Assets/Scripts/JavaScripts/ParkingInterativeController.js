158ab40fa1ec73cbbf4b38d0560a8ac75c2f1d24
Julian Wu <cosmobserver@gmail.com>
2012-12-03 11:07:11 -0500

    finished placement of parking obstacles and its physical behavior

--- Assets/Scripts/JavaScripts/ParkingInterativeController.js
+++ Assets/Scripts/JavaScripts/ParkingInterativeController.js
@@ -10,18 +10,10 @@ function Update () {
 
 }
 
-function OnTriggerEnter(other : Collider) {
-
-	Debug.Log("entering" + other.tag + " this: " + this.tag);
-	if (other.tag != "CarTriggerDetector") return;
-	if ( this.tag == "EnteringCourseInst" ) {
-		instruction_controller.instruction_content = "Good, you entering the course, please drive along the arrow on the course";
-	}
-	if (this.tag == "PrepareToNormalParkingInst") {
-		instruction_controller.instruction_content = "Now, try parking in the parking lot in front ";
-	}
-}
-
-function OnTriggerExit (other : Collider) {
-	
+function OnCollisionEnter(collision : Collision) {
+    // Debug-draw all contact points and normals
+    for (var contact : ContactPoint in collision.contacts)
+        Debug.DrawRay(contact.point, contact.normal, Color.white);
+    
+    
 }
